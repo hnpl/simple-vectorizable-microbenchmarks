0d3db57f64954314ac1c874f964562934d1ad9ed
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-03-29 17:08:10 -0700

    Add Makefiles for sgkernels
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- scatter_gather_kernels/scatter_gather_kernels_m5.cpp
+++ scatter_gather_kernels/scatter_gather_kernels_m5.cpp
@@ -10,6 +10,10 @@
 #include <cassert>
 #include <sys/time.h>
 
+#ifdef GEM5_ANNOTATION
+#include "gem5/m5ops.h"
+#endif
+
 #include "json.hpp"
 
 using json = nlohmann::json;
@@ -133,6 +137,10 @@ void executeKernels() {
     double t_total = 0;
     double t_start = 0;
     double t_end = 0;
+
+#ifdef GEM5_ANNOTATION
+    m5_exit(0);
+#endif
     for (auto const& k: kernels) {
         t_start = get_second();
         k.execute(dst, src);
@@ -141,6 +149,9 @@ void executeKernels() {
         std::cout << "Execute time: " << (t_end - t_start) << " seconds" << std::endl;
     }
     std::cout << "Total Elapsed Time: " << (t_total) << " seconds" << std::endl;
+#ifdef GEM5_ANNOTATION
+    m5_exit(0);
+#endif
 }
 
 int main() {
