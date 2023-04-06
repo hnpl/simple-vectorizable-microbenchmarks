eaf460cc95945687e6a213cf543816f1e21f9cd1
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-04-06 19:26:59 +0000

    Make the code a bit more readable
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- stream/stream_kernels.c
+++ stream/stream_kernels.c
@@ -18,46 +18,38 @@ double get_second()
 }
 
 
-double do_copy(TElement* __restrict__ dst, TElement* __restrict__ src, const size_t array_size)
+double do_copy(TElement* restrict dst, TElement* restrict src, const size_t array_size)
 {
     double t_start = get_second();
-    {
-        #pragma omp parallel for simd schedule(static)
-        for (int k = 0; k < array_size; k++)
-            dst[k] = src[k];
-    }
+    #pragma omp parallel for schedule(static)
+    for (size_t k = 0; k < array_size; k++)
+        dst[k] = src[k];
     return get_second() - t_start;
 }
 
-double do_scale(TElement* __restrict__ dst, TElement* __restrict__ src, const TElement scale_factor, const size_t array_size)
+double do_scale(TElement* restrict dst, TElement* restrict src, const TElement scale_factor, const size_t array_size)
 {
     double t_start = get_second();
-    {
-        #pragma omp parallel for simd schedule(static)
-        for (int k = 0; k < array_size; k++)
-            dst[k] = scale_factor * src[k];
-    }
+    #pragma omp parallel for
+    for (size_t k = 0; k < array_size; k++)
+        dst[k] = scale_factor * src[k];
     return get_second() - t_start;
 }
 
-double do_add(TElement* __restrict__ dst, TElement* __restrict__ src1, TElement* __restrict__ src2, const size_t array_size)
+double do_add(TElement* restrict dst, TElement* restrict src1, TElement* restrict src2, const size_t array_size)
 {
     double t_start = get_second();
-    {
-        #pragma omp parallel for simd schedule(static)
-        for (int k = 0; k < array_size; k++)
-            dst[k] = src1[k] + src2[k];
-    }
+    #pragma omp parallel for
+    for (size_t k = 0; k < array_size; k++)
+        dst[k] = src1[k] + src2[k];
     return get_second() - t_start;
 }
 
 double do_triad(TElement* restrict dst, TElement* restrict src1, TElement* restrict src2, const TElement scale_factor, const size_t array_size)
 {
     double t_start = get_second();
-    {
-        #pragma omp parallel for simd schedule(static)
-        for (int k = 0; k < array_size; k++)
-            dst[k] = src1[k] + scale_factor * src2[k];
-    }
+    #pragma omp parallel for
+    for (size_t k = 0; k < array_size; k++)
+        dst[k] = src1[k] + scale_factor * src2[k];
     return get_second() - t_start;
 }
